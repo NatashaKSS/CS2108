d4d66c03a3621503bc2cf9eb9693f05f1c7f1e4b
Adrian <A0122061@u.nus.edu>
2016-09-24 16:21:27 +0800

    adjusted keyword_matching.py

--- ImageSeach_demo/keyword_matching.py
+++ ImageSeach_demo/keyword_matching.py
@@ -8,6 +8,16 @@ flann_params = dict(algorithm = FLANN_INDEX_KDTREE,
                     trees = 4)
 list_surf_des = []
 list_match_result = []
+
+#To be used at initiation
+def loadSurf(file_name):
+    img1 = cv2.imread(file_name, 0)
+    detector = cv2.SURF()
+    kp1, des1 = detector.detectAndCompute(img1, None)
+    list_surf_des.append((file_name, des1))
+
+
+#To be used to obtain list of results
 def keyword_matching(query_image):
     img2 = cv2.imread(query_image, 0)  # trainImage
     detector = cv2.SURF()
@@ -29,12 +39,6 @@ def keyword_matching(query_image):
         for i, (m, n) in enumerate(matches):
             if m.distance < 0.7 * n.distance:
                 count += 1.0
-            score = max(count/len(des1), count/len(des2))
+        score = max(count/len(des1), count/len(des2))
         list_match_result.append([name, score])
-
-def loadSurf(file_name):
-    img1 = cv2.imread(file_name, 0)
-    detector = cv2.SURF()
-    kp1, des1 = detector.detectAndCompute(img1, None)
-    list_surf_des.append((file_name, des1))
-
+    return list_match_result
