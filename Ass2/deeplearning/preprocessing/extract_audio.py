10efee8a89cad909049a6b8b9163a48cd21a2465
NatashaKSS <xnagol@gmail.com>
2016-10-12 15:54:31 +0800

    Enabled extract_audio.py to extract .wav from all video files in directory

--- Ass2/deeplearning/preprocessing/extract_audio.py
+++ Ass2/deeplearning/preprocessing/extract_audio.py
@@ -1,20 +1,26 @@
 __author__ = "xiangwang1223@gmail.com"
+
 # The simple implementation of obtaining the audio clip of a original video.
-
+import os
 import moviepy.editor as mp
-
 
 def getAudioClip(video_reading_path, audio_storing_path):
     clip = mp.VideoFileClip(video_reading_path)
     clip.audio.write_audiofile(audio_storing_path)
 
-
 if __name__ == '__main__':
-    # 1. Set the access path to the original file.
-    video_reading_path = "../data/video/1.mp4"
+    list_of_video_names = os.listdir("../data/video")
+    print(list_of_video_names)
 
-    # 2. Set the path to store the extracted audio clip.
-    audio_storing_path = "../data/audio/1.wav"
+    for video_name in list_of_video_names:
+        # 1. Set the access path to the original file.
+        video_reading_path = "../data/video/" + video_name
+        print(video_reading_path)
 
-    # 3. Fetch and store the corresponding audio clip.
-    getAudioClip(video_reading_path=video_reading_path, audio_storing_path=audio_storing_path)
+        # 2. Set the path to store the extracted audio clip.
+        audio_storing_path = "../data/audio/" + video_name[:-4] + ".wav"
+        print(audio_storing_path)
+
+        # 3. Fetch and store the corresponding audio clip.
+        getAudioClip(video_reading_path=video_reading_path, \
+                     audio_storing_path=audio_storing_path)
