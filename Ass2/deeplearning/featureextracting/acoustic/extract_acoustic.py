192814f4086f118a04e363aa27987970e70666f4
Yuan Bin <chowwars@hotmail.com>
2016-10-15 23:22:27 +0800

    fix number of returned columns to 75

--- Ass2/deeplearning/featureextracting/acoustic/extract_acoustic.py
+++ Ass2/deeplearning/featureextracting/acoustic/extract_acoustic.py
@@ -12,9 +12,19 @@ from __future__ import print_function
 import moviepy.editor as mp
 import librosa
 import numpy as np
+import os
+import math
 
+# initialise boolean array for use later
+
+bool_array = []
+for i in range(75):
+    bool_array.append(True)
 
 def getAcousticFeatures(audio_reading_path):
+    hop_length = 2048 # default is 512, resulting in about 280+ columns max, maybe sticking to powers of 2 is better?
+
+    print("Processing:" + audio_reading_path)
     # 1. Load the audio clip;
     y, sr = librosa.load(audio_reading_path)
 
@@ -25,34 +35,58 @@ def getAcousticFeatures(audio_reading_path):
     tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr)
 
     # 4. Compute MFCC features from the raw signal.
-    feature_mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=512, n_mfcc=13)
+    feature_mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+    feature_mfcc = convertArray(feature_mfcc)
     print("MFCC Feature Done:", np.shape(feature_mfcc))
 
     # 5. Compute Melspectrogram features from the raw signal.
-    feature_spect = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=80000)
+    feature_spect = librosa.feature.melspectrogram(y=y, sr=sr, hop_length=hop_length, n_mels=128, fmax=80000)
+    feature_spect = convertArray(feature_spect)
     print("Melspectrogram Feature Done:", np.shape(feature_spect))
 
     # 6. Compute Zero-Crossing features from the raw signal.
-    feature_zerocrossing = librosa.feature.zero_crossing_rate(y=y)
+    feature_zerocrossing = librosa.feature.zero_crossing_rate(y=y, hop_length=hop_length)
+    feature_zerocrossing = convertArray(feature_zerocrossing)
     print("Zero-Crossing Rate:", np.shape(feature_zerocrossing))
 
     # 7. Compute Root-Mean-Square (RMS) Energy for each frame.
-    feature_energy = librosa.feature.rmse(y=y)
+    feature_energy = librosa.feature.rmse(y=y, hop_length=hop_length)
+    feature_energy = convertArray(feature_energy)
     print("Energy Feature:", np.shape(feature_energy))
+    
+    print()
 
     return feature_mfcc, feature_spect, feature_zerocrossing, feature_energy
 
 
+def convertArray(array):
+    num_cols = 75
+    
+    rows, cols = np.shape(array)
+    
+    num_repeats = math.ceil(75 / cols)
+    
+    new_array = np.tile(array, (1, num_repeats))
+    final_array = np.compress(bool_array, new_array, axis=1)
+    
+    return final_array
+
+
 if __name__ == '__main__':
-    # 1. Set the access path to the audio clip.
-    audio_reading_path = "../../data/audio/1.wav"
+    list_of_audio_names = os.listdir("../../data/audio")
+    print(list_of_audio_names)
+    
+    for audio_name in list_of_audio_names:
+        # 1. Set the access path to the audio clip.
+        audio_reading_path = "../../data/audio/" + audio_name
 
-    # 2. Fetch the corresponding features of the audio, consisting of mfcc, melspect, zero-crossing rate, and energy.
-    feature_mfcc, feature_spect, feature_zerocrossing, feature_energy = getAcousticFeatures(
-        audio_reading_path=audio_reading_path)
-	
-    # 3. Select the type of feature of interest, and set the storing path.
-    acoustic_storing_path = "../../feature/acoustic/1.csv"
+        # 2. Fetch the corresponding features of the audio, consisting of mfcc, melspect, zero-crossing rate, and energy.
+        feature_mfcc, feature_spect, feature_zerocrossing, feature_energy = getAcousticFeatures(
+            audio_reading_path=audio_reading_path)
+        """
+        # 3. Select the type of feature of interest, and set the storing path.
+        acoustic_storing_path = "../../feature/acoustic/" + audio_name[:-4] + ".csv"
 
-    # 4. Store the extracted acoustic feature(s) to .csv form.
-    np.savetxt(acoustic_storing_path, feature_mfcc, delimiter=",")
+        # 4. Store the extracted acoustic feature(s) to .csv form.
+        np.savetxt(acoustic_storing_path, feature_mfcc, delimiter=",")
+        """
