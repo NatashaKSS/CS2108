6c6d625786d8965749c457eff149f0d25d76b5ce
NatashaKSS <xnagol@gmail.com>
2016-10-23 02:38:06 +0800

    Revert "Revert "Added get audio venue functionality.""
    This reverts commit 77d27636d32431a1e0a786c4558dd2ff5a821ef0.

--- Ass2/deeplearning/preprocessing/extract_audio.py
+++ Ass2/deeplearning/preprocessing/extract_audio.py
@@ -9,18 +9,32 @@ def getAudioClip(video_reading_path, audio_storing_path):
     clip.audio.write_audiofile(audio_storing_path)
 
 if __name__ == '__main__':
-    list_of_video_names = os.listdir("../data/video")
+    list_of_video_names = os.listdir("../data/video_train_copy")
     print(list_of_video_names)
 
+    count = 0
+    num_exceptions = 0
+    excepted_vids = []
     for video_name in list_of_video_names:
         # 1. Set the access path to the original file.
-        video_reading_path = "../data/video/" + video_name
+        video_reading_path = "../data/video_train_copy/" + video_name
         print(video_reading_path)
 
         # 2. Set the path to store the extracted audio clip.
-        audio_storing_path = "../data/audio/" + video_name[:-4] + ".wav"
+        audio_storing_path = "../data/audio_adrian/" + video_name[:-4] + ".wav"
         print(audio_storing_path)
 
         # 3. Fetch and store the corresponding audio clip.
-        getAudioClip(video_reading_path=video_reading_path, \
-                     audio_storing_path=audio_storing_path)
+        try:
+            getAudioClip(video_reading_path=video_reading_path, \
+                         audio_storing_path=audio_storing_path)
+        except:
+            num_exceptions = num_exceptions + 1
+            excepted_vids.append(video_reading_path)
+            pass
+
+        count = count + 1
+        print(count, num_exceptions)
+        print()
+
+    print(excepted_vids)
