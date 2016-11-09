fc8f69a571185c97c4e1cc2a2c4907464a221fc3
NatashaKSS <xnagol@gmail.com>
2016-11-09 13:25:00 +0800

    Working copy of Assg 2 (needed to add more files)

--- Ass2/deeplearning/preprocessing/extract_audio.py
+++ Ass2/deeplearning/preprocessing/extract_audio.py
@@ -9,7 +9,7 @@ def getAudioClip(video_reading_path, audio_storing_path):
     clip.audio.write_audiofile(audio_storing_path)
 
 if __name__ == '__main__':
-    list_of_video_names = os.listdir("../data/video_train_copy")
+    list_of_video_names = os.listdir("../data/video")
     print(list_of_video_names)
 
     count = 0
@@ -17,11 +17,11 @@ if __name__ == '__main__':
     excepted_vids = []
     for video_name in list_of_video_names:
         # 1. Set the access path to the original file.
-        video_reading_path = "../data/video_train_copy/" + video_name
+        video_reading_path = "../data/video/" + video_name
         print(video_reading_path)
 
         # 2. Set the path to store the extracted audio clip.
-        audio_storing_path = "../data/audio_adrian/" + video_name[:-4] + ".wav"
+        audio_storing_path = "../data/audio_FINAL/" + video_name[:-4] + ".wav"
         print(audio_storing_path)
 
         # 3. Fetch and store the corresponding audio clip.
